7813f561ae144912c027ebb8cd30306b86d37359
Chadivesreenath <sreenath.chadive@gmail.com>
2023-03-28 20:00:52 +0530

    style and content fixes

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,7 @@
 export const aboutData = {
   title: "Who am I?",
-  description1: "My name's Chadive Sreenath. I'm a front-end web developer from Andhra Pradesh, India.",
-  description2: 'Result-oriented front-end developer, passionate about developing user-friendly web applications with the ability to write clean, reusable, and optimized code. I am willing to work in a challenging and competitive environment and wish to prove my talent for the growth of the organization',
+  description1: "I am Chadive Sreenath. I'm a front-end web developer from Andhra Pradesh, India.",
+  description2: 'Result-oriented front-end developer, passionate about developing user-friendly web applications with the ability to write clean, reusable, and optimized code. I am willing to work in a challenging and competitive environment and wish to prove my talent for the growth of the organization.',
+  description3: " I am a strong team player, and I thrive in collaborative environments where I can learn from my colleagues and contribute my own unique perspective which makes me a valuable addition to the organization.",
   image: 1,
 };
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,17 +1,16 @@
 export const educationData = [
   {
     id: 1,
-    institution: 'S.S.B.N Degree & P.G. College',
-    course: 'Bachelor of Science (Stats) ',
+    institution: 'Sri Krishnadevaraya University, Anantapur',
+    course: 'Bachelor of Science (Statistics) ',
     startYear: '2018',
     endYear: '2021'
-},
-{
+  },
+  {
     id: 2,
-    institution: 'Sree Masters Junior College',
-    course: '12th (maths, physics and chemistry)',
+    institution: 'Sree Masters Junior College, Anantapur',
+    course: '12th (Maths, Physics and Chemistry)',
     startYear: '2016',
     endYear: '2018'
-}
+  }
 ];
- 
